bb72cb610a1e533b34c8ce4dc6f72a1a4b83feff
Nayden Luiz dos Santos <naydenluiz@hotmail.com>
2018-05-03 14:23:48 -0300

    Subi código de exemplo de consumo de API SOAP via C#

---
+++ smsServiceCSharp/smsServiceCSharp/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace smsServiceCSharp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Button4 = new System.Windows.Forms.Button();
+            this.Button1 = new System.Windows.Forms.Button();
+            this.Label2 = new System.Windows.Forms.Label();
+            this.Button2 = new System.Windows.Forms.Button();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.Button3 = new System.Windows.Forms.Button();
+            this.txtMensagem = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.GroupBox1 = new System.Windows.Forms.GroupBox();
+            this.GroupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // Button4
+            // 
+            this.Button4.Location = new System.Drawing.Point(61, 96);
+            this.Button4.Name = "Button4";
+            this.Button4.Size = new System.Drawing.Size(109, 23);
+            this.Button4.TabIndex = 15;
+            this.Button4.Text = "Checar Créditos";
+            this.Button4.UseVisualStyleBackColor = true;
+            // 
+            // Button1
+            // 
+            this.Button1.Location = new System.Drawing.Point(6, 19);
+            this.Button1.Name = "Button1";
+            this.Button1.Size = new System.Drawing.Size(86, 36);
+            this.Button1.TabIndex = 0;
+            this.Button1.Text = "Ver Recebidas";
+            this.Button1.UseVisualStyleBackColor = true;
+            // 
+            // Label2
+            // 
+            this.Label2.AutoSize = true;
+            this.Label2.Location = new System.Drawing.Point(12, 19);
+            this.Label2.Name = "Label2";
+            this.Label2.Size = new System.Drawing.Size(59, 13);
+            this.Label2.TabIndex = 14;
+            this.Label2.Text = "Mensagem";
+            // 
+            // Button2
+            // 
+            this.Button2.Location = new System.Drawing.Point(98, 19);
+            this.Button2.Name = "Button2";
+            this.Button2.Size = new System.Drawing.Size(116, 36);
+            this.Button2.TabIndex = 1;
+            this.Button2.Text = "Ver Recebidas Data";
+            this.Button2.UseVisualStyleBackColor = true;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(12, 41);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(49, 13);
+            this.Label1.TabIndex = 13;
+            this.Label1.Text = "Números";
+            // 
+            // Button3
+            // 
+            this.Button3.Location = new System.Drawing.Point(349, 41);
+            this.Button3.Name = "Button3";
+            this.Button3.Size = new System.Drawing.Size(75, 23);
+            this.Button3.TabIndex = 12;
+            this.Button3.Text = "Enviar";
+            this.Button3.UseVisualStyleBackColor = true;
+            this.Button3.Click += new System.EventHandler(this.Button3_Click_1);
+            // 
+            // txtMensagem
+            // 
+            this.txtMensagem.Location = new System.Drawing.Point(77, 12);
+            this.txtMensagem.Name = "txtMensagem";
+            this.txtMensagem.Size = new System.Drawing.Size(266, 20);
+            this.txtMensagem.TabIndex = 11;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.Location = new System.Drawing.Point(77, 38);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(266, 20);
+            this.txtNumero.TabIndex = 10;
+            // 
+            // GroupBox1
+            // 
+            this.GroupBox1.Controls.Add(this.Button1);
+            this.GroupBox1.Controls.Add(this.Button2);
+            this.GroupBox1.Location = new System.Drawing.Point(231, 70);
+            this.GroupBox1.Name = "GroupBox1";
+            this.GroupBox1.Size = new System.Drawing.Size(228, 67);
+            this.GroupBox1.TabIndex = 9;
+            this.GroupBox1.TabStop = false;
+            this.GroupBox1.Text = "Checar Recebimento";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(525, 140);
+            this.Controls.Add(this.Button4);
+            this.Controls.Add(this.Label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.Button3);
+            this.Controls.Add(this.txtMensagem);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.GroupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.GroupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        internal System.Windows.Forms.Button Button4;
+        internal System.Windows.Forms.Button Button1;
+        internal System.Windows.Forms.Label Label2;
+        internal System.Windows.Forms.Button Button2;
+        internal System.Windows.Forms.Label Label1;
+        internal System.Windows.Forms.Button Button3;
+        internal System.Windows.Forms.TextBox txtMensagem;
+        internal System.Windows.Forms.TextBox txtNumero;
+        internal System.Windows.Forms.GroupBox GroupBox1;
+    }
+}
+
---
+++ smsServiceCSharp/smsServiceCSharp/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace smsServiceCSharp
+{
+    public partial class Form1 : Form
+    {
+        public System.DateTime dtInicial = System.DateTime.Parse("01/02/2018");
+
+        public System.DateTime dtFinal = System.DateTime.Parse("11/02/2018");
+        public Form1()
+        {
+            InitializeComponent();
+            Load += Form1_Load;
+        }
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+            //'Testa se o web service responde e dar as mensagens de boas vindas no Cabeçalho do form
+            try
+            {
+                //Instancia do WebService
+                wsSMS.SMSServiceSoapClient sms = new wsSMS.SMSServiceSoapClient();
+                // E necessário pois algumas vezes o tempo de resposta do webservice pode ser maior do que 
+                // a que o sistema aceita esperar
+                System.Net.ServicePointManager.Expect100Continue = false;
+
+                this.Text = sms.envioTeste();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao Acessar webService:" + ex.Message);
+            }
+
+        }
+
+
+
+        private void Button1_Click(System.Object sender, System.EventArgs e)
+        {
+            wsSMS.SMSServiceSoapClient sms = new wsSMS.SMSServiceSoapClient();
+            string strretorno = null;
+            //Pega o retorno em String de todas as mensagens recebidas
+            strretorno = sms.ChecarRecebidas("seu@email.com", "12345", dtInicial, dtFinal);
+
+            MessageBox.Show(strretorno);
+        }
+
+        private void Button2_Click(System.Object sender, System.EventArgs e)
+        {
+            wsSMS.SMSServiceSoapClient sms = new wsSMS.SMSServiceSoapClient();
+            System.Data.DataTable dtretorno = null;
+            //
+            //Pega as mensagens recebidas em um DataTable
+            dtretorno = sms.ChecarRecebidasData("seu@email.com", "12345", dtInicial, dtFinal);
+            if ((dtretorno != null))
+            {
+                MessageBox.Show("Data table retornou " + dtretorno.Rows.Count + " registros.");
+            }
+            else
+            {
+                MessageBox.Show("Data table não retornou registros.");
+            }
+
+        }
+
+        private void Button3_Click(System.Object sender, System.EventArgs e)
+        {
+            try
+            {
+                wsSMS.SMSServiceSoapClient sms = new wsSMS.SMSServiceSoapClient();
+                long StrRetorno = 0 ;
+                //Realiza o envio de mensagem e pega o retorno.
+                
+                StrRetorno = sms.envioMessagem("seu@email.com", "12345", txtMensagem.Text, txtNumero.Text, "1");
+                
+                MessageBox.Show("Retorno:" + StrRetorno);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao enviar mensagem: " + ex.Message);
+            }
+        }
+
+        private void Button4_Click(System.Object sender, System.EventArgs e)
+        {
+            string qtdCreditos = null;
+            wsSMS.SMSServiceSoapClient sms = new wsSMS.SMSServiceSoapClient();
+            System.Net.ServicePointManager.MaxServicePointIdleTime = 240;
+            System.Net.ServicePointManager.Expect100Continue = true;
+            //o webservice demora mais tempo pra processar a quantidade de créditos do que as demais funções, por isso aumento o timeout de espera pra 240.
+            qtdCreditos = sms.verificaCredito("seu@email.com", "12345");
+            MessageBox.Show(" Créditos:" + qtdCreditos);
+        }
+
+        private void Button3_Click_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
